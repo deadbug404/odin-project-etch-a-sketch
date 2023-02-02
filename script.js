86e30c8df64ad7109782674c11061cae01e39a62
nannoisdead <narisafeint@gmail.com>
2023-02-02 21:41:55 +0800

    add grid settings

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let container = document.querySelector('.container');
+let grid = 16;
 
 //creates the grid
-for(let i =1; i<= 16;i++){
-    let strip = document.createElement('div');
-    strip.classList.add('strip');
-    container.appendChild(strip);
-    for(let j = 1; j <= 16;j++){
-        let div = document.createElement('div');
-        div.classList.add('grid');
-        strip.appendChild(div);
+function createGrid(size){
+    for(let i =1; i<= size;i++){
+        let strip = document.createElement('div');
+        strip.classList.add('strip');
+        container.appendChild(strip);
+        for(let j = 1; j <= size;j++){
+            let div = document.createElement('div');
+            div.classList.add('grid');
+            strip.appendChild(div);
+        }
     }
 }
 
-//adds color to the specific div when hovered
-let grids = document.querySelectorAll('.grid');
-grids.forEach((grid) => {
-    grid.addEventListener('mouseover', function (event){
+//sets a listener to change the color a specific grid
+function setGridListeners(){
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach((grid) => {
+        grid.addEventListener('mouseover', function (event){
         grid.classList.add('active');
     });
 });
+}
+
+//change the grid 
+function setGrid(){
+    container.textContent = '';
+    let gridByUser = document.getElementById('gridByUser').value;
+    createGrid(gridByUser);
+    setGridListeners();
+}
+
+createGrid(grid);
+setGridListeners();
