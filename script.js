b6e10be723c1e52f9a750f0e6b2317d4cbe68d13
nannoisdead <narisafeint@gmail.com>
2023-02-03 18:38:05 +0800

    add the ability to show grid and change color of paint

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let container = document.querySelector('.container');
+let gridStyle = document.getElementsByClassName('grid');
+let colorPicker = document.getElementById('pcolor');
 let grid = 16;
 
 //creates the grid
@@ -29,9 +31,39 @@ function setGridListeners(){
 function setGrid(){
     container.textContent = '';
     let gridByUser = document.getElementById('gridByUser').value;
-    createGrid(gridByUser);
-    setGridListeners();
+    if(gridByUser > 100  || gridByUser < 1){
+        alert("Only enter a number ranging from 1-100");
+    }else{
+        createGrid(gridByUser);
+        setGridListeners();
+    }
+}
+
+//shows the grid or not
+function showGrid(checkbox){
+    if(checkbox.checked){
+        for(let i = 0; i < gridStyle.length; i++){
+            gridStyle[i].style.border = '1px solid black';
+        }
+    }else{
+        for(let i = 0; i < gridStyle.length; i++){
+            gridStyle[i].style.border = '';
+        }
+    }
+}
+
+//sets the color of the brush
+function changeColor(event){
+    let grids = document.querySelectorAll('.grid');
+    grids.forEach((grid) => {
+        grid.addEventListener('mouseover', function (){
+        grid.setAttribute('style',`background-color: ${event.target.value};`);
+        console.log(event.target.value);
+    });
+});
 }
 
 createGrid(grid);
 setGridListeners();
+colorPicker.addEventListener('input',changeColor);
+colorPicker.select();
